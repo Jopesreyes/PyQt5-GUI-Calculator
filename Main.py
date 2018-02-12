e904fa95ae93fe383114d1657f9bdc7be90ac45b
sukhendu11 <32995580+sukhendu11@users.noreply.github.com>
2018-02-13 00:47:26 +0600

    Delete Main.py

--- Main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from mycalculator import CalculatorWindow
-
-app = QApplication(sys.argv)
-
-calculator = CalculatorWindow()
-
-sys.exit(app.exec_())
